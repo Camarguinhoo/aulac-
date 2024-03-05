1d8bf3c5efd224c7ad9419124cae266bed8d1de0
Leandro Camargo <145075769+Camarguinhoo@users.noreply.github.com>
2024-03-04 21:46:29 -0300

    primeiro projeto

---
+++ primeiroprojeto/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Criar um objeto cliente
+//Classe obj new Classe()
+Cliente objCliente = new Cliente();
+objCliente.Nome = "Pedro";
+objCliente.Cpf = "888";
+Console.WriteLine(objCliente.Cpf);
+
+//Console.WriteLine("Hello, World!");
+ 
